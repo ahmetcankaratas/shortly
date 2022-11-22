ca4a81ca74aee9420a7e3269affc2dd53e1a4774
Ahmet Can Karataş <karatas@yahoo.com>
2022-11-22 13:59:58 +0300

    add shorten form

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // ### Primary
-
         cyan: "hsl(180, 66%, 49%)",
+        cyanLight: "hsl(180, 66%, 69%)",
         darkViolet: "hsl(257, 27%, 26%)",
-
-        // ### Secondary
-
         red: "hsl(0, 87%, 67%)",
-
-        // ### Neutral
-
-        gray: "hsl(0, 0%, 75%)",
         grayishViolet: "hsl(257, 7%, 63%)",
         veryDarkBlue: "hsl(255, 11%, 22%)",
         veryDarkViolet: "hsl(260, 8%, 14%)",
